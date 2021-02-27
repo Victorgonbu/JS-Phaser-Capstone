4cddfd2f2cf18b8923145e582313344c03a57055
victorG <peter200970@hotmail.com>
2021-02-26 22:13:33 -0500

    Refactor leaderboard code

--- src/Scenes/Leaderboard.js
+++ src/Scenes/Leaderboard.js
@@ -13,13 +13,14 @@ export default class Leaderboard extends Phaser.Scene {
       });
   }
 
-  create () {
-    let config = this.cache.json.get('gamma_json');
-    this.cache.bitmapFont.add('gamma', Phaser.GameObjects.RetroFont.Parse(this, config));
-    this.text = this.add.bitmapText(this.sys.game.config.width/2 - 176/2, 10, 'gamma', 'LEADERBOARD');
-    console.log(this.text.width);
-    let scores = this.requestScores();
-    scores.then((response) => {
+  displayScore(user, verticalSpace, i) {
+      if(i > 10) return
+      this.add.bitmapText(this.sys.game.config.width - 700, verticalSpace, 'gamma', `${i}. ${user.user.toUpperCase()}`);
+      this.add.bitmapText(this.sys.game.config.width - 150, verticalSpace, 'gamma', `${user.score}`);
+  }
+
+  processRequest(request) {
+    request.then((response) => {
         return response.json();
     }).then((response) => {
         return response.result.sort((a, b) => {
@@ -30,15 +31,20 @@ export default class Leaderboard extends Phaser.Scene {
         let verticalSpace = 60
         response.forEach(user => {
         
-            if(i < 11) {
-                this.add.bitmapText(this.sys.game.config.width - 700, verticalSpace, 'gamma', `${i}. ${user.user.toUpperCase()}`);
-                this.add.bitmapText(this.sys.game.config.width - 150, verticalSpace, 'gamma', `${user.score}`);
-            }
-           
+           this.displayScore(user, verticalSpace, i);
             verticalSpace += 30;
             i += 1;
         });
     });
+  }
+
+  create () {
+    let config = this.cache.json.get('gamma_json');
+    this.cache.bitmapFont.add('gamma', Phaser.GameObjects.RetroFont.Parse(this, config));
+    this.text = this.add.bitmapText(this.sys.game.config.width/2 - 176/2, 10, 'gamma', 'LEADERBOARD');
+    
+    let scoresRequest = this.requestScores();
+    this.processRequest(scoresRequest)
 
     this.titleButton = new Button(this, this.sys.game.config.width/2, this.sys.game.config.height- 80, 'blueButton1', 'blueButton2', 'Menu', 'Title');
 
